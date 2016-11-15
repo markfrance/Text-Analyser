5b3268b215599cf934a42c8ff7919a6f638c9e0e
Mark France <markusfrance@hotmail.com>
2016-11-15 00:15:29 +0000

    Initial commit

---
+++ IntelligentEditing/Controllers/HomeController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Web.Mvc;
+using IntelligentEditing.Models;
+
+namespace IntelligentEditing.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Upload( HttpPostedFileBase upload)
+        {
+            var mimeTypes = new string[]{ "text/plain"};
+            var minWordCount = TextFile.GetMinimumWordCount();
+
+            if (upload == null)
+            {
+                ModelState.AddModelError("TextFile", "ERROR: Please upload file");
+            }
+            else if (!mimeTypes.Contains(upload.ContentType))
+            {
+                ModelState.AddModelError("TextFile", "ERROR: Please upload a valid text file.");
+            }
+            else
+            {
+                var text = new TextFile
+                {
+                    FileName = System.IO.Path.GetFileName(upload.FileName),
+                };
+
+                using (var reader = new StreamReader(upload.InputStream))
+                {
+                    text.Content = reader.ReadToEnd();
+                    
+                    if (!text.IsEnglish())
+                        ModelState.AddModelError("TextFile", "ERROR: File contains non english characters");
+
+                    if (text.GetWordCount() < minWordCount)
+                        ModelState.AddModelError("TextFile", $"ERROR: File contains less than {minWordCount} words");
+
+                    if (ModelState.IsValid)
+                    {
+                        var fileName = Path.GetFileName(upload.FileName);
+                        var path = Path.Combine(GetUploadFolder(), fileName);
+                        upload.SaveAs(path);
+
+                        ViewBag.Message = $"{text.FileName} - [Is English = {text.IsEnglish().ToString()}] - Word Count - {text.GetWordCount()}] - [Repeated Phrases - {text.GetRepeatedPhraseDescription()}]";
+
+                        RedirectToAction("Index");
+                    }
+
+                }
+            }
+            
+                return View("Index");
+
+        }
+
+        private string GetUploadFolder()
+        {
+            return Server.MapPath(ConfigurationManager.AppSettings["Upload.Folder"]);
+        }
+
+        public ActionResult History()
+        {
+            var uploadedFiles = new List<TextFile>();
+ 
+            var files = Directory.GetFiles(GetUploadFolder());
+ 
+            foreach(var file in files)
+            {
+                var fileInfo = new FileInfo(file);
+ 
+                uploadedFiles.Add ( new TextFile
+                {
+                    FileName = fileInfo.Name,
+                    Date = fileInfo.CreationTime,
+                    Content = System.IO.File.ReadAllText(fileInfo.FullName)
+                });
+
+            }
+
+            return View(uploadedFiles);
+        }
+
+    }
+}
---
+++ IntelligentEditing/Models/TextFile.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace IntelligentEditing.Models
+{
+    public class TextFileModel
+    {
+        [Required]
+        public HttpPostedFileBase TextFile { get; set; }
+    }
+
+    public class RepeatedPhrase
+    {
+        public string Phrase { get; set; }
+        public int Count { get; set; }
+        public string Description { get {return $"[{Phrase} (x {Count})]"; } }
+    }
+
+    public class TextFile
+    {
+        public DateTime Date { get; set; }
+        public string FileName { get; set; }
+        public string Content { get; set; }
+        
+        /// <summary>
+        /// Returns whether the text content only contains English characters
+        /// </summary>
+        public bool IsEnglish()
+        {
+            Regex regex = new Regex("[A-Za-z0-9 .,-=+%£(){}\\[\\]\\\'\\s\"@]");
+            MatchCollection matches = regex.Matches(Content);
+        
+            return matches.Count.Equals(Content.Length);
+        }
+
+        /// <summary>
+        /// Returns number of words in file
+        /// </summary>
+        public int GetWordCount()
+        {
+            return Content.Split(new char[] { ' ', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+        
+        /// <summary>
+        /// Gets string description of repeated phrases
+        /// </summary>
+        public string GetRepeatedPhraseDescription()
+        {
+            return string.Join("\n", FindRepeatedPhrases().Select(r => r.Description));
+        }
+
+        /// <summary>
+        /// Gets minimum word count from web.config
+        /// </summary>
+        public static int GetMinimumWordCount()
+        {
+            return Convert.ToInt32(ConfigurationManager.AppSettings["Minimum.Word.Count"]);
+        }
+        /// <summary>
+        /// Gets phrase length from web.config
+        /// </summary>
+        private int GetMinimumPhraseLength()
+        {
+            return Convert.ToInt32(ConfigurationManager.AppSettings["Repeated.Phrase.Length"]);
+        }
+
+        /// <summary>
+        /// Returns list of repeated phrases 
+        /// </summary>
+        private List<RepeatedPhrase> FindRepeatedPhrases()
+        {
+            var split = Content.Split(' ');
+            var PhraseLength = GetMinimumPhraseLength();
+
+            List<string> words = new List<string>();
+            
+            words.AddRange(split
+                .Select((x, i) => new { Value = x, Index = i })
+                .Select(x => Combine(split, x.Index, x.Index + PhraseLength - 1)));
+            
+            return words
+                .GroupBy(x => x)
+                .Where(x => x.Count() > 1)
+                .Select(x => new RepeatedPhrase{ Phrase = x.Key, Count = x.Count() }).ToList();
+        }
+
+        private string Combine(IEnumerable<string> source, int startIndex, int endIndex)
+        {
+            return string.Join(" ", source.Where((x, i) => i >= startIndex && i <= endIndex));
+        }
+        
+    }
+}
